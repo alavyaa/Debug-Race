a1a85f3cc47da09f9f1ae3be474cd2bd70646db2
Alavya Bajpai <bajpaialavya@gmail.com>
2026-03-07 04:18:56 +0530

    Update authenticateUser.middleware.js

--- backend/src/middleware/authenticateUser.middleware.js
+++ backend/src/middleware/authenticateUser.middleware.js
@@ -3,17 +3,28 @@ const User = require("../models/User.model");
 
 const authenticateUser = async (req, res, next) => {
   try {
+
+    // TOKEN FROM COOKIE
     let token = req.cookies.token;
+
+    // ALSO CHECK AUTH HEADER
+    if (!token && req.headers.authorization) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
     if (!token) {
       return res.status(401).json({ message: "Not authorized !" });
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
-    req.user = await User.findById(decoded.id);
+
+    req.user = await User.findById(decoded.id).select("-password");
+
     next();
+
   } catch (error) {
-    return res.status(400).json({message : "Something went wrong!", error});
+    return res.status(401).json({ message: "Invalid token", error });
   }
 };
 
-module.exports =  authenticateUser ;
+module.exports = authenticateUser;
