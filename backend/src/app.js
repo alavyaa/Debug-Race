60354f19c6c715b759c4c11c3ec7cba08d962454
ADi <adiiibariya@gmail.com>
2026-03-07 13:23:06 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "https://debug-race-ztam.onrender.com",
     /https:\/\/debug-race.*\.vercel\.app/
   ],
   credentials: true
