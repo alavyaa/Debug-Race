6da1b226d95ef6754f5aec9c374cb5d0b0d76a4d
ADi <adiiibariya@gmail.com>
2026-03-08 22:54:45 +0530

    Update api.service.js

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "debug-race-production-5710.up.railway.app"; // Update this to your server URL
+const API_URL = import.meta.env.VITE_API_URL || "https://debug-race-production-5710.up.railway.app"; // Update this to your server URL must add https://
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
