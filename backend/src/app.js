9b74a034598f2bbae233ef3e3bbbd4c6a68d7ad9
Alavya Bajpai <bajpaialavya@gmail.com>
2026-03-09 22:29:10 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://debug-racee.onrender.com"
+    "https://debug-race-ztam.onrender.com"
   ],
   credentials: true
 }));
